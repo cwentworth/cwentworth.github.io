74efa6d559980036c518f6dc52723c67aeac5ad1
cwentworth <christopher.wentworth@gmail.com>
2017-12-02 12:54:50 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="cstyle.css">
+<title>chris wentworth</title>
+</head>
+<body>
+<?php include_once("analyticstracking.php") ?>
+<div id="top">
+<div class="left"><a href="http://www.chriswentworth.com"><h1>chris wentworth</h1></a></div>
+<div class="right">
+<a href="http://www.linkedin.com/in/christopherwentworth/en">about</a>
+<a href="portfolio.php">portfolio</a>      
+</div>
+</div>
+<div id="main"></div>
+</body>
+</html>
