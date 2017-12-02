2883808b4548a7ff33901426ea68989a36ec9716
cwentworth <christopher.wentworth@gmail.com>
2017-12-02 18:40:42 -0500

    adding portfolio page

---
+++ portfolio.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="cstyle.css">
+</head>
+<body>
+<?php include_once("analyticstracking.php") ?>
+<div id="top">
+<div class="left"><h1><a href="http://www.chriswentworth.com">chris wentworth</a></h1></div>
+<div class="right">
+<a href="http://www.linkedin.com/in/christopherwentworth/en">about</a>
+<a href="portfolio.php">portfolio</a>    
+</div>
+</div>
+<div id="portfolio"><p>iVantage Health Analytics <br><span id="portfoliosubtext">Interactive page showing state-level impact on Critical Access Hospitals from proposed changes to swing bed reimbursement policies.<br>utilizes html, css, javascript, php, and mysql.</span></p>
+<div id="portfolioimage"><a href="portfolio/ivantage/oigswingbedanalysis.php"><img width="500px" src="images/ivantageanalysis.jpg"/></a></div>
+<p>SARSSM <br><span id="portfoliosubtext">Informational page with pledge form and counter for SARSSM's Light the Way campaign.<br>utilizes html, css, javascript, php, and mysql.</span></p>
+<div id="portfolioimage"><a href="http://sarsonline.org/light-the-way.php"><img width="500px" src="images/sarssmltw.jpg"/></a></div>
+<p>Union Bagel<br><span id="portfoliosubtext">Concept page for a local bagel shop in Portland Maine.<br>utilizes html and css.</span></p>
+<div id="portfolioimage"><a href="portfolio/unionbagel/unionbagel.html"><img width="500px" src="images/unionbagel.jpg"/></a></div>
+<p>Column to List Generator<br><span id="portfoliosubtext">Simple utility that converts column data separated by carriage returns to a list separated by a user-defined delimiter.<br>utilizes html, css, and javascript.</span></p>
+<div id="portfolioimage"><a href="portfolio/utilities/columntolist/columntolist.html"><img width="500px" src="images/commalistmaker.png"/></a></div>
+<br>
+<br>
+<br>
+</div>
+<div id="main"></div>
+</body>
+</html>
