2d24bc16722c7503721c067ea54e884c7fc491a0
chris wentworth <christopher.wentworth@gmail.com>
2019-08-06 20:13:36 -0400

    Committed

---
+++ ff-draft-order-calculator/doc.js
@@ -0,0 +1,16 @@
+var teams = ['team12','team11',
+'team10','team9',
+'team8','team7',
+'team6','team5',
+'team4','team3',
+'team2','team1'
+];
+
+var weights = [
+12,11,
+10,9,
+8,7,
+6,5,
+4,3,
+2,1
+]
