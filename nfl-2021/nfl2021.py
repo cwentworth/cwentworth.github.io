bea96edef0179122fcaf0625fb129939074b1372
chris wentworth <christopher.wentworth@gmail.com>
2021-10-18 22:26:42 -0400

    update2021-10-18

--- nfl-2021/nfl2021.py
+++ nfl-2021/nfl2021.py
@@ -405,7 +405,7 @@ updateField.decompose()
 headTwo = hc.find('h2',attrs={'id':'secondH'})
 headTwo.decompose()
 
-ts.style.set_properties(**{'text-align': 'center'})
+ts.style.set_properties(**{'text-align': 'center', 'data-ordering':'false'})
 picksTableHtml = ts.to_html(table_id='teamPicks',classes=['compact', 'hover', 'stripe', 'sorting_disabled'],justify='center',index=False,escape=False)
 picksTableHtmls = BeautifulSoup(picksTableHtml,features="lxml")
 
