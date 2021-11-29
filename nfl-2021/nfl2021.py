5c64fd5d3b936ea48ad27dc8cd0deb68a74f3f3f
chris wentworth <christopher.wentworth@gmail.com>
2021-11-28 20:23:38 -0500

    update2021-11-28

--- nfl-2021/nfl2021.py
+++ nfl-2021/nfl2021.py
@@ -497,7 +497,7 @@ csFinal = csFinal[[
 ]]
 
 plt.style.use('dark_background')
-pp = csFinal.plot(style={'Chris':'Red','Dennis':'Teal','Matt':'Blue'},linestyle='-',marker='$•$')
+pp = csFinal.plot(style={'Chris':'#AA0000','Dennis':'Teal','Matt':'Blue'},linestyle='-',marker='$•$')
 plt.savefig('imgs/progress.png')
 
 finalOutput = pandas.merge(susChris,susDennis,on='Week',how='left')
